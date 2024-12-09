8e19cb4ca8f4852ec04e886e8c359f774e29773b
Anum Mustafa <106741994+Anum-Mustafa@users.noreply.github.com>
2024-12-09 16:17:19 +0500

    Create day2_list.py

---
+++ day2_list.py
@@ -0,0 +1,35 @@
+# Creating a list of fruits
+fruits = ["apple", "banana", "cherry"]
+print("Fruits List:", fruits)
+
+
+ #Example 2
+# Accessing elements by index
+fruits = ["apple", "banana", "cherry"]
+print("First Fruit:", fruits[0])  # apple
+print("Last Fruit:", fruits[-1])  # cherry
+
+
+#Example 3
+# Changing a value in the list
+fruits = ["apple", "banana", "cherry"]
+fruits[1] = "blueberry"
+print("Updated Fruits List:", fruits)
+
+
+#Example 4
+# Slicing a list
+numbers = [10, 20, 30, 40, 50]
+print("First Three Numbers:", numbers[:3])
+print("Last Two Numbers:", numbers[-2:])
+
+#Example 5
+# Finding the length of a list
+fruits = ["apple", "banana", "cherry"]
+print("Number of Fruits:", len(fruits))
+
+
+
+
+
+
