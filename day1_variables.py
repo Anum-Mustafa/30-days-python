8d1ebb25554d3e13a57961a70d95ae169f213b06
Anum Mustafa <106741994+Anum-Mustafa@users.noreply.github.com>
2024-12-09 16:00:26 +0500

    Create day1_variables.py

---
+++ day1_variables.py
@@ -0,0 +1,33 @@
+# Day 1: Variables in Python
+
+# Defining variables
+name = "Anum Mustafa" # String
+age = 24  # Integer
+height = 5.8  # Float
+is_student = False  # Boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height, "feet")
+print("Is Student:", is_student)
+
+# Example 2
+# Combining strings using variables
+first_name = "Anum"
+last_name = "Mustafa"
+full_name = first_name + " " + last_name
+
+print("Full Name:", full_name)
+
+#Example 3
+# Performing arithmetic operations with variables
+x = 15
+y = 4
+
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+print("Floor Division:", x // y)
+print("Modulus:", x % y)
+print("Exponentiation:", x ** y)
