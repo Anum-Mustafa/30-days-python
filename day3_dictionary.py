1a1b9d605e30815f667e1feff3f1316711e850f1
Anum Mustafa <106741994+Anum-Mustafa@users.noreply.github.com>
2024-12-09 16:23:37 +0500

    Create day3_dictionary.py

---
+++ day3_dictionary.py
@@ -0,0 +1,23 @@
+# Creating a dictionary
+person = {
+    "name": "Anum",
+    "age": 24,
+    "city": "Karachi",
+    "gender": "Female"  
+}
+print("Person Dictionary:", person)
+
+
+#Example 2
+# Adding a new key-value pair
+person["job"] = "Engineer"
+print("After Adding Job:", person)
+
+# Updating an existing value
+person["age"] = 26
+print("After Updating Age:", person)
+
+
+#Example 3
+# Finding the number of items in a dictionary
+print("Number of items in dictionary:", len(person))
